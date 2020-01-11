1039fd61fb408fedb721216d16ea5921029a93be
Mohsen Razani <mohsenrazani.z@gmail.com>
2020-01-11 17:00:55 +0330

    memory v 1.0.1

--- src/Component/Game.jsx
+++ src/Component/Game.jsx
@@ -1,42 +1,49 @@
 import React, { Component } from "react";
 import { Icon, Alert, notification } from "antd";
-import classes from "../Styles/main.module.css";
+import classes from "../Styles/Game.module.css";
 import _ from "underscore";
-
+const itemstest = [
+  "alert",
+  "bulb",
+  "car",
+  "frown",
+  "experiment",
+  "copyright",
+  "thunderbolt",
+  "skin",
+  "scissor",
+  "slack",
+  "alert",
+  "bulb",
+  "car",
+  "frown",
+  "experiment",
+  "copyright",
+  "thunderbolt",
+  "skin",
+  "scissor",
+  "slack"
+];
 class Game extends Component {
   state = {
-    cards: [
-      { id: 1, name: "alert", eq: false, clicked: false },
-      { id: 2, name: "bulb", eq: false, clicked: false },
-      { id: 3, name: "car", eq: false, clicked: false },
-      { id: 4, name: "frown", eq: false, clicked: false },
-      { id: 5, name: "experiment", eq: false, clicked: false },
-      { id: 6, name: "bulb", eq: false, clicked: false },
-      { id: 7, name: "car", eq: false, clicked: false },
-      { id: 8, name: "alert", eq: false, clicked: false },
-      { id: 9, name: "experiment", eq: false, clicked: false },
-      { id: 10, name: "frown", eq: false, clicked: false },
-      { id: 11, name: "copyright", eq: false, clicked: false },
-      { id: 12, name: "thunderbolt", eq: false, clicked: false },
-      { id: 13, name: "skin", eq: false, clicked: false },
-      { id: 14, name: "thunderbolt", eq: false, clicked: false },
-      { id: 15, name: "copyright", eq: false, clicked: false },
-      { id: 16, name: "scissor", eq: false, clicked: false },
-      { id: 17, name: "skin", eq: false, clicked: false },
-      { id: 18, name: "slack", eq: false, clicked: false },
-      { id: 19, name: "slack", eq: false, clicked: false },
-      { id: 20, name: "scissor", eq: false, clicked: false }
-    ],
     pickedItem: [],
     items: [],
     counter: 0,
     hint: false,
-    hintCounter:0
+    hintCounter: 0,
   };
   componentDidMount() {
-    this.setState({
-      items: _.sample(this.state.cards, 20)
-    });
+    let id = 1;
+    for (let step = 0; step < itemstest.length; step++) {
+       this.setState(prev => {
+        return {
+          items: _.sample([
+            ...prev.items,
+            { id: id++, name: itemstest[step], eq: false, clicked: false }
+          ],20)
+        };
+      });
+    }
   }
   Winner = (value, index, array) => {
     return value === true;
@@ -145,48 +152,65 @@ class Game extends Component {
     });
   };
   status = counter => {
-    let point = (((100/counter).toFixed(2))*100) - (this.state.hintCounter *20)
+    let point = (100 / counter).toFixed(2) * 100 - this.state.hintCounter * 20;
     switch (true) {
       case point > 220:
-        return  "Point :" + point + "  (WOW)";
+        return "Point :" + point + "  (WOW)";
         break;
       case point > 180:
-        return  "Point :" + point + "  (So Good)";
+        return "Point :" + point + "  (So Good)";
         break;
       case point > 160:
         return "Point :" + point + "  (Not Bad)";
         break;
       case point > 140:
-        return  "Point :" + point + "  (Increase your effort)";
+        return "Point :" + point + "  (Increase your effort)";
         break;
       default:
-        return  "Point :" + point + "  (You Need Brain)";
+        return "Point :" + point + "  (You Need Brain)";
     }
   };
   Retry = () => {
+    let id = 1;
     this.setState({
       counter: 0,
-      items: _.sample(this.state.cards, 20),
-      pickedItem: []
+      pickedItem: [],
+      items:[]
+    },()=>{
+      for (let step = 0; step < itemstest.length; step++) {
+        this.setState(prev => {
+          return {
+            items: _.sample([
+              ...prev.items,
+              { id: id++, name: itemstest[step], eq: false, clicked: false }
+            ],20)
+          };
+        });
+      }
     });
   };
   Hint = () => {
-    this.setState({
-      hint : true,
-      hintCounter:this.state.hintCounter+1,
-      items:this.state.items.map((items, i) => {
-        if (items.clicked === false)
-          return { ...items, clicked: true };
-        else return items;
-      })
-    },()=>setTimeout(()=>{
-      this.setState({hint:false,items:this.state.items.map((items, i) => {
-          if (items.eq !== true)
-            return { ...items, clicked: false };
+    this.setState(
+      {
+        hint: true,
+        hintCounter: this.state.hintCounter + 1,
+        items: this.state.items.map((items, i) => {
+          if (items.clicked === false) return { ...items, clicked: true };
           else return items;
-        })})
-    },2000))
-  }
+        })
+      },
+      () =>
+        setTimeout(() => {
+          this.setState({
+            hint: false,
+            items: this.state.items.map((items, i) => {
+              if (items.eq !== true) return { ...items, clicked: false };
+              else return items;
+            })
+          });
+        }, 2000)
+    );
+  };
   render() {
     // console.log('pickedItem:' ,this.state.pickedItem);
     // console.log("items:", this.state.items);
@@ -213,11 +237,13 @@ class Game extends Component {
               {this.state.items.map((i, index) => (
                 <button
                   disabled={i.eq ? true : false}
-                  key={index}
-                  className={i.eq ? classes.itemEQ : classes.itemNEQ}
+                  key={i.id}
+                  id={i.eq ? classes.itemEQ : classes.itemNEQ}
+                  className={classes.item}
                   onClick={() => this.ItemClicked(i, index)}
                 >
                   <Icon
+                      key={i.id}
                     type={i.name}
                     className={i.eq ? classes.BtnIconEQ : classes.BtnIconNEQ}
                     style={{
@@ -232,42 +258,53 @@ class Game extends Component {
             <div className={classes.LastMove}>
               <span className={classes.LastMoveTxt}>Last :</span>
               <span>
-            {this.state.pickedItem.map(i => (
-                <Icon
+                {this.state.pickedItem.map(i => (
+                  <Icon
+                      key={i.id}
                     type={i.name}
                     className={classes.LastMoveIcon}
                     style={{
                       display: "block"
                     }}
-                />
-            ))}
-            </span>
+                  />
+                ))}
+              </span>
             </div>
             <div className={classes.counter}>
               <span style={{ color: "rgb(76,60,37)" }}>Moves :</span>
               <span style={{ paddingLeft: 15, color: "rgb(229,180,112)" }}>
-              {this.state.counter}
-            </span>
+                {this.state.counter}
+              </span>
             </div>
             <div>
               <div className={classes.hintCounter}>
                 <span style={{ color: "rgb(76,60,37)" }}>Hint :</span>
                 <span style={{ paddingLeft: 15, color: "rgb(229,180,112)" }}>
-              {this.state.hintCounter}
-            </span>
+                  {this.state.hintCounter}
+                </span>
               </div>
             </div>
           </div>
-          {
-            this.state.hint ?
-                <div className={classes.hintMsg}>
-                  <span>Save In Memory</span>
-                </div>
-             :
-                <div className={classes.hint} style={{display:`${this.state.items.map(i => i.eq).every(this.Winner) ? 'none' :'block'}`}}>
-                  <button className={classes.hintBtn} onClick={this.Hint}>Hint</button>
-                </div>
-          }
+          {this.state.hint ? (
+            <div className={classes.hintMsg}>
+              <span>Save In Memory</span>
+            </div>
+          ) : (
+            <div
+              className={classes.hint}
+              style={{
+                display: `${
+                  this.state.items.map(i => i.eq).every(this.Winner)
+                    ? "none"
+                    : "block"
+                }`
+              }}
+            >
+              <button className={classes.hintBtn} onClick={this.Hint}>
+                Hint
+              </button>
+            </div>
+          )}
         </div>
       </>
     );
